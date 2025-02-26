2daa94d4aa43eed7487033cb48d6c68d8660cc63
Kallol7 <kalloldhar7@gmail.com>
2025-02-27 01:11:37 +0600

    Update main.go

--- main.go
+++ main.go
@@ -7,5 +7,9 @@ import (
 )
 
 func main() {
-	fmt.Println("Length of text:", pyutil.Len("bigtext"))
+	strLen := pyutil.Len("Some Text")
+	fmt.Println("Length of text:", strLen) // Length of text: 9
+
+	floatToStr := pyutil.Str(3.141592653589793)
+	fmt.Printf("Number: %v, Type: %[1]T", floatToStr) // Number: 3.141592653589793, Type: string
 }
